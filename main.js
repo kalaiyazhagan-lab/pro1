5976b25c7a77288b51207b34111fe3229edafcde
kalaiyazhagan-lab <bujjikutty0597@gmail.com>
2025-08-17 15:13:35 +0530

    testing

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-const express = require("express");
-const PORT = 3000;
-const app = express()
-const {form} = require("./form.js")
-
-app.use(express.json())
-
-app.get("/",(req,res)=>{
-    res.send(form);
-})
-
-app.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`)
-})
+console.log("hello");
